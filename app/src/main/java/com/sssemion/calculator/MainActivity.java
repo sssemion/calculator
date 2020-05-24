98226d139ddcfe566ee5039d04adc6e1b3bbcab5
Fill <zubareff10@gmail.com>
2020-05-25 03:40:00 +0500

    working calculator. Next stop is make him better.

--- app/src/main/java/com/sssemion/calculator/MainActivity.java
+++ app/src/main/java/com/sssemion/calculator/MainActivity.java
@@ -3,12 +3,126 @@ package com.sssemion.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultField; // текстовое поле для вывода результата
+    TextView numberField;   // поле для ввода числа
+    TextView operationField;    // текстовое поле для вывода знака операции
+    Double operand = null;  // операнд операции
+    String lastOperation = "="; // последняя операция
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultField = (TextView) findViewById(R.id.resultField);
+        numberField = (TextView) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if (operand != null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    // получение ранее сохраненного состояния
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand = savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+
+    public void onOperationClick(View view) {
+        Button button = (Button) view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        // если введенно что-нибудь
+        if (number.length() > 0) {
+            number = number.replace(',', '.');
+            try {
+                performOperation(Double.valueOf(number), op);
+            } catch (NumberFormatException ex) {
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    public void onNumberClick(View view) {
+        Button button = (Button) view;
+        numberField.append(button.getText());
+
+        if (lastOperation.equals("=") && operand != null) {
+            operand = null;
+        }
+    }
+
+    private void performOperation(Double number, String operation) {
+
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if (operand == null) {
+            operand = number;
+        } else {
+            if (lastOperation.equals("=")) {
+                lastOperation = operation;
+            }
+            switch (lastOperation) {
+                case "=":
+                    operand = number;
+                    break;
+                case "/":
+                    if (number == 0) {
+                        operand = 0.0;
+                    } else {
+                        operand /= number;
+                    }
+                    break;
+                case "*":
+                    operand *= number;
+                    break;
+                case "+":
+                    operand += number;
+                    break;
+                case "-":
+                    operand -= number;
+                    break;
+                case "%":
+                    operand *= number / 100;
+                    break;
+            }
+        }
+        BigDecimal bd = new BigDecimal(operand.toString());
+        double out = bd.setScale(6, RoundingMode.DOWN).doubleValue();
+        resultField.setText(Double.toString(out).replace('.', ','));
+        numberField.setText("");
+    }
+
+    public void onClearClick(View view) {
+        operand = null;
+        lastOperation = "=";
+        numberField.setText("");
+        operationField.setText("");
+        resultField.setText("");
+    }
+
+    public void onSignClick(View view) {
+        if (numberField.getText().toString().startsWith("-"))
+            numberField.setText(numberField.getText().toString().replace("-", ""));
+        else
+            numberField.setText("-" + numberField.getText().toString());
     }
 }
